49c27ceb369a4e3872a4e27237fe2078294c598a
houkai <hikoukay@163.com>
2020-01-15 00:34:44 +0800

    1

---
+++ online-taxi-config-server/src/test/java/com/houkai/configserver/OnlineTaxiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houkai.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTaxiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
